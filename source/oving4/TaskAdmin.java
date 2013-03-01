7e86bf9eaf2add7440202dbb40d0556ff6407d49
Jørgen Nordmoen <nordmoen90@gmail.com>
2013-03-01 15:05:25 +0100

    Added a simple parseMathOperator to the TA

--- source/oving4/TaskAdmin.java
+++ source/oving4/TaskAdmin.java
@@ -5,4 +5,25 @@ import jade.core.Agent;
 @SuppressWarnings("serial")
 public class TaskAdmin extends Agent {
 
+	public static MathOperator parseOperator(String op){
+		char convertedOp = 'N';
+		String trimedOp = op.trim();
+		if(trimedOp.length() == 1){
+			convertedOp = trimedOp.charAt(0);
+		}
+		
+		switch (convertedOp) {
+		case '+':
+			return MathOperator.PLUS;
+		case '-':
+			return MathOperator.MINUS;
+		case '*':
+			return MathOperator.MULTIPLY;
+		case '/':
+			return MathOperator.DIVIDE;
+		default:
+			throw new RuntimeException("Could not parse '" + op + 
+					"' into a proper MathOperator");
+		}
+	}
 }
