2ec505c5a1db88a4890c171cf95e2d513ad2d9ad
Jørgen Nordmoen <nordmoen90@gmail.com>
2013-03-01 15:37:42 +0100

    Added a check for leaf in the Abstract solver so that complex expressions can not be solved by a basic solver

--- source/oving4/AbstractSolver.java
+++ source/oving4/AbstractSolver.java
@@ -6,36 +6,42 @@ import java.util.ArrayList;
 
 @SuppressWarnings("serial")
 public abstract class AbstractSolver extends Agent {
-	
+
 	protected final ArrayList<MathOperator> solverTypes = new ArrayList<MathOperator>();
-	
+
 	public AbstractSolver(MathOperator type) {
 		this.solverTypes.add(type);
 	}
-	
+
 	public AbstractSolver(ArrayList<MathOperator> types){
 		this.solverTypes.addAll(types);
 	}
-	
+
 	public AbstractSolver(MathOperator[] types){
 		for(MathOperator op : types){
 			solverTypes.add(op);
 		}
 	}
-	
+
 	public ArrayList<MathOperator> getSolverTypes(){
 		return this.solverTypes;
 	}
-	
+
 	public Number solve(MathProblem<Number, Number> problem){
 		if(this.solverTypes.contains(problem.getOp())){
-			return this.subSolve(problem);
+			if(problem.isLeaf()){
+				return this.subSolve(problem);
+			}else{
+				throw new RuntimeException("A Solver can only solve basic " +
+						"math expressions not complex expressions. " +
+						"Expression was: " + problem);
+			}
 		}
 		throw new RuntimeException("This agent can't solve this type of " +
 				"problem. Problem was: " + problem + 
 				", this solver can solve: " + solverTypes);
 	}
-	
+
 	abstract protected Number subSolve(MathProblem<Number, Number> problem);
 
 }
